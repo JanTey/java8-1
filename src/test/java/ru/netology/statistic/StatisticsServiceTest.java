75a36cbdae97f5588b9247b4b37aa6eb4cc67c4f
Zhan Teyvans <teyvans@gmail.com>
2023-04-13 18:51:31 +0300

    Added tests for full coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,88 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfNegative() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-12, 5, 8, 4, -5, 3, 8, 6, -11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfOne() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfNegativeAll() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-12, -5, -8, -4, -5, -3, -8, -6, -11, -11, -12};
+        long expected = -3;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+//    @Test
+//    void findMaxIfZero() {
+//        StatisticsService service = new StatisticsService();
+//
+//        long[] incomesInBillions = {};
+//        long expected = 0;
+//
+//        long actual = service.findMax(incomesInBillions);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+
+    @Test
+    void findMaxMaxFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {15, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxEnd() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
+        long expected = 30;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
